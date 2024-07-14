9c67ede4d4f0039e6630fa6a5f3d445b0b07a1b9
Ojas <143781753+ojas-17@users.noreply.github.com>
2024-07-14 12:33:27 +0000

    captcha solver

--- captcha.py
+++ captcha.py
@@ -1,5 +1,6 @@
 import pytesseract
 from PIL import Image, ImageFilter, ImageEnhance, ImageOps
+import re
 
 # Load the image
 image_path = "cap.png"
@@ -12,13 +13,13 @@ def preprocess_image(image):
     
     # Enhance the image contrast
     enhancer = ImageEnhance.Contrast(gray_image)
-    enhanced_image = enhancer.enhance(3)
+    enhanced_image = enhancer.enhance(2)
     
     # Apply a median filter to reduce noise
-    filtered_image = enhanced_image.filter(ImageFilter.MedianFilter(size=5))
+    filtered_image = enhanced_image.filter(ImageFilter.MedianFilter(size=3))
     
     # Binarize the image
-    threshold = 128
+    threshold = 100
     binary_image = filtered_image.point(lambda p: p > threshold and 255)
     
     return binary_image
@@ -34,4 +35,7 @@ print(f"Processed image saved at: {processed_image_path}")
 # Use pytesseract to do OCR on the processed image
 captcha_text = pytesseract.image_to_string(processed_image, config='--psm 6').strip()
 
-print(f"Captcha: {captcha_text}")
+# print(f"Captcha: {captcha_text}")
+
+captcha_text_alphanumeric = re.sub(r'\W+', '', captcha_text)
+print(f"Captcha: {captcha_text_alphanumeric}")
