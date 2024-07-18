7b9a83cf4f16026bf155cf2a0af31d3becd0b656
Ojas <143781753+ojas-17@users.noreply.github.com>
2024-07-18 06:23:26 +0000

    captcha

--- captcha.py
+++ captcha.py
@@ -30,18 +30,18 @@ processed_image = preprocess_image(image)
 # Save the processed image (optional)
 processed_image_path = "processed_image.png"
 processed_image.save(processed_image_path)
-print(f"Processed image saved at: {processed_image_path}")
+# print(f"Processed image saved at: {processed_image_path}")
 
 # Use pytesseract to do OCR on the processed image
 captcha_text = pytesseract.image_to_string(processed_image, config='--psm 6').strip()
 
 # Post-process to extract only alphanumeric characters
 captcha_text_alphanumeric = re.sub(r'\W+', '', captcha_text)
-print(captcha_text_alphanumeric)
+# print(captcha_text_alphanumeric)
 
 # Write the cleaned text to a new text file
 output_text_file = "output.txt"
 with open(output_text_file, 'w') as file:
     file.write(captcha_text_alphanumeric)
 
-print(f"Captcha text written to: {output_text_file}")
+# print(f"Captcha text written to: {output_text_file}")
